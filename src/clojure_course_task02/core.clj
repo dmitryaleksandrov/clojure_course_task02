0239e6203c242e33f2ac7fee0369f0daa4474f87
Dmitry Aleksandrov <dmitry.aleksandrov@gmail.com>
2013-03-27 20:51:34 +0400

    second homework

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,20 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
+(import java.io.File)
 
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (let [fpattern (re-pattern file-name)]
+    (letfn [(find-in-dir [d]
+              (let [fs (filter #(.isFile %) (.listFiles d))
+                    ds (filter #(.isDirectory %) (.listFiles d))] 
+                (apply concat 
+                  (conj (map find-in-dir ds) 
+                    (filter #(re-matches fpattern %)  
+                      (conj (map #(.getName %) fs) (.getPath d)))))))]
+    (let [result (find-in-dir (File. path))]
+      (shutdown-agents)
+      result))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
